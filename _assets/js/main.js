25ca41e8ddd8edc9723e1412ced151e3cb29cc43
crshd <christian.brassat@gmail.com>
2013-12-03 20:37:27 +0800

    i'm going through changes. chaaaaangeees

--- _assets/js/main.js
+++ _assets/js/main.js
@@ -1,1 +1,17 @@
+$(document).ready(function() {
 
+	/*
+	 * Fixed navigation
+	 */
+	var navOffset = $('nav').offset().top;
+	console.log(navOffset);
+
+	$(window).scroll(function() {
+		if($(window).scrollTop() > navOffset) {
+			$('nav').addClass('fixed');
+		} else {
+			$('nav').removeClass('fixed');
+		}
+	});
+
+});
